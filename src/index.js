7131715c0cb52e26fe48f694c4b99142f790a0b3
Moudipa01 <moudipa.jana2020@vitstudent.ac.in>
2023-09-02 22:03:14 +0530

    chat-app

---
+++ src/index.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import './index.css';
+import App from './App';
+import { ChakraProvider } from '@chakra-ui/react';
+import { HashRouter } from 'react-router-dom';
+import ChatProvider from './Context/ChatProvider';
+
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <HashRouter>
+    <ChatProvider>
+      <ChakraProvider>
+        <App />
+      </ChakraProvider>
+    </ChatProvider>
+  </HashRouter>,
+  document.getElementById("root")
+);
+
